84118e5a6c117e7ad1c243e9b431c30c6ae90a16
aksh-t <tanno.ao@gmail.com>
2020-05-08 02:04:36 +0900

    volumeCredits変数をインライン化することで変数削除する

--- src/main.js
+++ src/main.js
@@ -60,9 +60,7 @@ module.exports = function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 };
