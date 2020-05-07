904ac7a269a98ef25fca5ff299a521e6722cf6c0
aksh-t <tanno.ao@gmail.com>
2020-05-07 13:59:06 +0900

    testでassertする

--- test/main.js
+++ test/main.js
@@ -1,3 +1,4 @@
+const assert = require('assert').strict;
 const fs = require("fs");
 const path = require("path");
 const statement = require("../src/main");
@@ -14,7 +15,15 @@ const invoice = invoices[0];
 
 function testStatement() {
   const result = statement(invoice, plays);
-  console.log(result);
+  const expect =
+    `Statement for BigCo
+  Hamlet: $650.00 (55 seats)
+  As You Like It: $580.00 (35 seats)
+  Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`;
+  assert.equal(result, expect);
 }
 
 testStatement();
