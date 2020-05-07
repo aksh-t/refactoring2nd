cbb76badfe22c6c16787c89c387e92638b07a790
aksh-t <tanno.ao@gmail.com>
2020-05-08 01:48:16 +0900

    volumeCreditsFor関数の処理結果変数名をresultに変更

--- src/main.js
+++ src/main.js
@@ -27,10 +27,10 @@ module.exports = function statement(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
-    return volumeCredits;
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
   }
 
   let totalAmount = 0;
