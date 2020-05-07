074471b7409cea4a2e5acf9d4d2b4db91d62521a
aksh-t <tanno.ao@gmail.com>
2020-05-08 00:57:14 +0900

    statement関数のthisAmount変数をインライン化

--- src/main.js
+++ src/main.js
@@ -37,17 +37,15 @@ module.exports = function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
-
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 喜劇の時は10人につき、さらにポイントを加算
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
     // 注文の内訳を出力
-    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
       } seats)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
