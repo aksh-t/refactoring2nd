5c1f1efe02da1340e88fbd1d6f2ca714e46cb44a
aksh-t <tanno.ao@gmail.com>
2020-05-08 01:47:44 +0900

    volumeCreditsFor関数の仮引数名を意図が伝わる名前に変更

--- src/main.js
+++ src/main.js
@@ -26,10 +26,10 @@ module.exports = function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function volumeCreditsFor(perf) {
+  function volumeCreditsFor(aPerformance) {
     let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
     return volumeCredits;
   }
 
