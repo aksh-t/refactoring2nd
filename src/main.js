1bfe07486b82e56164ac9add84853bd6c2aecbb1
aksh-t <tanno.ao@gmail.com>
2020-05-08 00:17:31 +0900

    とりあえず演目ごとの料金計算のロジックをamountFor関数として囲う

--- src/main.js
+++ src/main.js
@@ -13,22 +13,24 @@ module.exports = function statement(invoice, plays) {
     const play = plays[perf.playID];
     let thisAmount = 0;
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
+    function amountFor() {
+      switch (play.type) {
+        case "tragedy":
+          thisAmount = 40000;
+          if (perf.audience > 30) {
+            thisAmount += 1000 * (perf.audience - 30);
+          }
+          break;
+        case "comedy":
+          thisAmount = 30000;
+          if (perf.audience > 20) {
+            thisAmount += 10000 + 500 * (perf.audience - 20);
+          }
+          thisAmount += 300 * perf.audience;
+          break;
+        default:
+          throw new Error(`unknown type: ${play.type}`);
+      }
     }
 
     // ボリューム特典のポイントを加算
