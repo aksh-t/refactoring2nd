0ac78816e351d830fa8b859bda6c70b34dd3c329
aksh-t <tanno.ao@gmail.com>
2020-05-08 22:01:26 +0900

    ループを分割

--- src/main.js
+++ src/main.js
@@ -57,6 +57,8 @@ module.exports = function statement(invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
       } seats)\n`;
+  }
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 
