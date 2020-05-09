ce8af6f5ed15db703bdf7a0b177551839f7a97ba
aksh-t <tanno.ao@gmail.com>
2020-05-09 23:45:55 +0900

    戻り値用の変数の名前をresultにする

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -1,11 +1,11 @@
 // 今のところnodeで直接実行したいので、テキストのES2015形式ではなくCommonJS形式でexport/importする
 module.exports = function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
