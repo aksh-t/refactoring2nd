be337201b99e835302d465950af985c66c925e6f
aksh-t <tanno.ao@gmail.com>
2020-05-09 22:16:17 +0900

    中間データ構造を作成するロジックを独立した関数にする

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 module.exports = function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+};
+
+function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, invoice, plays);
+  return statementData;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
