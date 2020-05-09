f50b08a772ccfe9ceea28e1e16005227aad5864f
aksh-t <tanno.ao@gmail.com>
2020-05-09 23:52:23 +0900

    HTML版の出力関数でも使うので、usd関数をトップレベルに移動

--- src/statement.js
+++ src/statement.js
@@ -16,12 +16,12 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
 };
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
