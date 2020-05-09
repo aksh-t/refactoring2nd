f06a65c6d53dfb5db067d275a0c9d8a22af6ef2f
aksh-t <tanno.ao@gmail.com>
2020-05-10 00:39:53 +0900

    PerformanceCalculatorクラスの導入

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -8,6 +8,7 @@ module.exports = function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance);
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
@@ -56,3 +57,9 @@ module.exports = function createStatementData(invoice, plays) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 };
+
+class PerformanceCalculator {
+  constructor(aPerformance) {
+    this.performances = aPerformance;
+  }
+}
