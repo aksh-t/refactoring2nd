0fb82f193d660ce207945ec85dda4b1854b0859e
aksh-t <tanno.ao@gmail.com>
2020-05-10 00:45:31 +0900

    amountFor関数をPerformanceCalculatorクラスにコピー

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -63,4 +63,26 @@ class PerformanceCalculator {
     this.performances = aPerformance;
     this.play = aPlay;
   }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.audience > 30) {
+          result += 1000 * (this.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.audience > 20) {
+          result += 10000 + 500 * (this.audience - 20);
+        }
+        result += 300 * this.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  }
 }
