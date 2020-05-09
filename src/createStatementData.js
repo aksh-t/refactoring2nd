ebdd79f44cc9ac9f0d39247b14755aafc16e953a
aksh-t <tanno.ao@gmail.com>
2020-05-10 01:08:31 +0900

    ファクトリメソッドで演劇の種類ごとにサブクラスを生成し分ける

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -30,7 +30,12 @@ module.exports = function createStatementData(invoice, plays) {
 };
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`未知の演劇の種類： ${aPlay.type}`);
+  }
 }
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
@@ -66,4 +71,10 @@ class PerformanceCalculator {
     if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
     return result;
   }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+}
+
+class ComedyCalculator extends PerformanceCalculator {
 }
