d66542591e3cdd50abd4cd9ee13dfce0b832085b
aksh-t <tanno.ao@gmail.com>
2020-05-14 02:16:22 +0900

    Expectスタイル

--- chap4/test/test.js
+++ chap4/test/test.js
@@ -1,10 +1,11 @@
 // var assert = require('assert');
-var assert = require('chai').assert
+// var assert = require('chai').assert;
+var expect = require('chai').expect;
 const { Province, sampleProvinceData } = require("../src/main");
 
 describe('province', function () {
   it('shortfall', function () {
     const asia = new Province(sampleProvinceData());
-    assert.equal(asia.shortfall, 5);
+    expect(asia.shortfall).equal(5);
   });
 });
