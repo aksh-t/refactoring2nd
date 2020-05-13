4955f2792d0ab74d5c17f50601dedabe51952130
aksh-t <tanno.ao@gmail.com>
2020-05-14 02:19:14 +0900

    本に合わせてexpect形式に戻す

--- chap4/test/test.js
+++ chap4/test/test.js
@@ -1,13 +1,13 @@
 // var assert = require('assert');
 // var assert = require('chai').assert;
-// var expect = require('chai').expect;
-var should = require('chai').should();
+// var should = require('chai').should();
+var expect = require('chai').expect;
 const { Province, sampleProvinceData } = require("../src/main");
 
 describe('province', function () {
   it('shortfall', function () {
     const asia = new Province(sampleProvinceData());
-    // expect(asia.shortfall).equal(5);
-    asia.shortfall.should.equal(5);
+    // asia.shortfall.should.equal(5);
+    expect(asia.shortfall).equal(5);
   });
 });
