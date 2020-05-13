2497fccd9b5f512dd44322f237347ea0bc76af00
aksh-t <tanno.ao@gmail.com>
2020-05-14 02:18:36 +0900

    Shouldスタイル

--- chap4/test/test.js
+++ chap4/test/test.js
@@ -1,11 +1,13 @@
 // var assert = require('assert');
 // var assert = require('chai').assert;
-var expect = require('chai').expect;
+// var expect = require('chai').expect;
+var should = require('chai').should();
 const { Province, sampleProvinceData } = require("../src/main");
 
 describe('province', function () {
   it('shortfall', function () {
     const asia = new Province(sampleProvinceData());
-    expect(asia.shortfall).equal(5);
+    // expect(asia.shortfall).equal(5);
+    asia.shortfall.should.equal(5);
   });
 });
