4fee93342162974f8dabd4b7c93efa9f1e095dd2
aksh-t <tanno.ao@gmail.com>
2020-05-14 22:32:43 +0900

    フィクスチャの共通化のダメな例

--- chap4/test/test.js
+++ chap4/test/test.js
@@ -5,10 +5,13 @@ var expect = require('chai').expect;
 const { Province, sampleProvinceData } = require("../src/main");
 
 describe('province', function () {
+  const asia = new Province(sampleProvinceData()); // これはやってはいけない
   it('shortfall', function () {
-    const asia = new Province(sampleProvinceData());
     // assert.equal(asia.shortfall, 5);
     // asia.shortfall.should.equal(5);
     expect(asia.shortfall).equal(5);
+  });
+  it('profit', function () {
+    expect(asia.profit).equal(230);
   });
 });
