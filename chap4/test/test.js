9b3b80a4b13ea3aafb64c043616c205fd097f8f0
aksh-t <tanno.ao@gmail.com>
2020-05-15 00:20:22 +0900

    境界値の検査

--- chap4/test/test.js
+++ chap4/test/test.js
@@ -22,4 +22,51 @@ describe('province', function () {
     expect(asia.shortfall).equal(-6);
     expect(asia.profit).equal(292);
   });
+  it('zero demand', function () {
+    asia.demand = 0;
+    expect(asia.shortfall).equal(-25);
+    expect(asia.profit).equal(0);
+  });
+  it('negative demand', function () {
+    asia.demand = -1;
+    expect(asia.shortfall).equal(-26);
+    expect(asia.profit).equal(-10);
+  });
+  it('empty string demand', function () {
+    asia.demand = "";
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+  });
+});
+
+describe('no producers', function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20
+    };
+    noProducers = new Province(data);
+  });
+  it('shortfall', function () {
+    expect(noProducers.shortfall).equal(30);
+  });
+  it('profit', function () {
+    expect(noProducers.profit).equal(0);
+  });
+});
+
+describe('string for producers', function () {
+  it('', function () {
+    const data = {
+      name: "String Producers",
+      producers: "",
+      demand: 30,
+      price: 20
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).equal(0);
+  });
 });
