f88414938284461671be000fe6c9a45b6ceb273e
aksh-t <tanno.ao@gmail.com>
2020-05-21 00:04:17 +0900

    変数の抽出: 同じ式を変数に置き換える

--- catalog/chap6/ExtractVariable/example/main.js
+++ catalog/chap6/ExtractVariable/example/main.js
@@ -3,5 +3,5 @@ module.exports = function price(order) {
   const basePrice = order.quantity * order.itemPrice;
   return basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    Math.min(basePrice * 0.1, 100);
 }
