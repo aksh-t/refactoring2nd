a00323a4bc74bc09fe4c5e0ccff75168bf0aba7e
aksh-t <tanno.ao@gmail.com>
2020-05-21 00:03:00 +0900

    変数の抽出: 作成した変数で元の式を置き換える

--- catalog/chap6/ExtractVariable/example/main.js
+++ catalog/chap6/ExtractVariable/example/main.js
@@ -1,7 +1,7 @@
 module.exports = function price(order) {
   // price = base price - quantity discount + shipping
   const basePrice = order.quantity * order.itemPrice;
-  return order.quantity * order.itemPrice -
+  return basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
     Math.min(order.quantity * order.itemPrice * 0.1, 100);
 }
