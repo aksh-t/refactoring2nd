e66d646e3c6649c41d1a2fbfaea07f5ba14be14a
aksh-t <tanno.ao@gmail.com>
2020-05-24 03:28:45 +0900

    関数宣言の変更: 移行的手順の例のスタート地点とテスト

---
+++ catalog/chap6/ChangeFunctionDeclaration/examples/RenamingAFunctionMigrationMechanics/test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const circum = require('./main');
+
+describe('circum', function () {
+  it('', function () {
+    expect(circum(1)).to.equal(6.283185307179586);
+  });
+});
