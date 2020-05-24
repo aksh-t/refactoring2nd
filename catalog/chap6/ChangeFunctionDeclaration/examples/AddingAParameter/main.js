0ca916a8b830460637e73910bf1a892349bf5ba4
aksh-t <tanno.ao@gmail.com>
2020-05-25 02:43:44 +0900

    古い関数の呼び出し元がなくなったので、関数自体削除する

--- catalog/chap6/ChangeFunctionDeclaration/examples/AddingAParameter/main.js
+++ catalog/chap6/ChangeFunctionDeclaration/examples/AddingAParameter/main.js
@@ -7,9 +7,6 @@ module.exports = class Book {
   get reservations() {
     return this._reservations;
   }
-  addReservation(customer) {
-    this.zz_addReservation(customer, false);
-  }
   zz_addReservation(customer, isPriority) {
     assert(isPriority === true || isPriority === false);
     this._reservations.push(customer);
