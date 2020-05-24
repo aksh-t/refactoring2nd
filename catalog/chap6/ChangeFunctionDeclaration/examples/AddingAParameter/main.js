7f2638b6a14ff6a54b7c42aa1bf3e75198cae34a
aksh-t <tanno.ao@gmail.com>
2020-05-25 02:37:38 +0900

    関数宣言の変更: パラメータ追加の例のスタート地点

---
+++ catalog/chap6/ChangeFunctionDeclaration/examples/AddingAParameter/main.js
@@ -0,0 +1,11 @@
+module.exports = class Book {
+  constructor() {
+    this._reservations = [];
+  }
+  get reservations() {
+    return this._reservations;
+  }
+  addReservation(customer) {
+    this._reservations.push(customer);
+  }
+}
---
+++ catalog/chap6/ChangeFunctionDeclaration/examples/AddingAParameter/test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+const Book = require('./main');
+
+describe('Book', () => {
+  it('', () => {
+    const book = new Book();
+    const customer = { name: "Peter" };
+    book.addReservation(customer);
+    expect(book.reservations).to.deep.equal([customer]);
+  });
+});
