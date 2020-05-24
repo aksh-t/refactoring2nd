e326561c8250c1680da65ed792ec955488fe6a57
aksh-t <tanno.ao@gmail.com>
2020-05-25 02:39:29 +0900

    関数の抽出

--- catalog/chap6/ChangeFunctionDeclaration/examples/AddingAParameter/main.js
+++ catalog/chap6/ChangeFunctionDeclaration/examples/AddingAParameter/main.js
@@ -6,6 +6,9 @@ module.exports = class Book {
     return this._reservations;
   }
   addReservation(customer) {
+    this.zz_addReservation(customer);
+  }
+  zz_addReservation(customer) {
     this._reservations.push(customer);
   }
 }
