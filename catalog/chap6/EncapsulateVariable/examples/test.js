6998c5dcc60db9d807d657e4b196d63b73c670f3
aksh-t <tanno.ao@gmail.com>
2020-05-29 02:17:15 +0900

    テスト追加

--- catalog/chap6/EncapsulateVariable/examples/test.js
+++ catalog/chap6/EncapsulateVariable/examples/test.js
@@ -1,8 +1,22 @@
-const expect = require('chai').expect;
+const { expect, assert } = require('chai');
 const spaceship = require('./main');
+const { defaultOwner, setDefaultOwner } = require('./defaultOwner');
 
 describe('spaceship', () => {
   it('', () => {
     expect(spaceship.owner).to.deep.equal({ firstName: "Rebecca", lastName: "Parsons" });
   });
 });
+
+// memo: このテストは失敗する
+// なぜなら、main.jsでdefaultOwnerが書き変わってしまっているから
+// describe('defaultOwner', () => {
+//   it('', () => {
+//     const owner1 = defaultOwner();
+//     console.log("owner1", owner1);
+//     assert.equal("Fowler", owner1.lastName, "when set");
+//     const owner2 = defaultOwner();
+//     owner2.lastName = "Parsons";
+//     assert.equal("Parsons", owner1.lastName, "after change owner2"); // これでいい？
+//   });
+// });
