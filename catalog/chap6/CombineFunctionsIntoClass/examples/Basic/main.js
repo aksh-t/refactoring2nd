015f2d1443e07ddb29b9e6117a41da2a62c4d5e4
aksh-t <tanno.ao@gmail.com>
2020-06-04 00:11:06 +0900

    レコードのカプセル化によりReadingクラスにデータをまとめる

--- catalog/chap6/CombineFunctionsIntoClass/examples/Basic/main.js
+++ catalog/chap6/CombineFunctionsIntoClass/examples/Basic/main.js
@@ -33,3 +33,16 @@ exports.client3 = function clinet3() {
     return baseRate(aReading.month, aReading.year) * aReading.quantity;
   }
 };
+
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() { return this._customer; }
+  get quantity() { return this._quantity; }
+  get month() { return this._month; }
+  get year() { return this._year; }
+}
