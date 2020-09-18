fee7237a1b163527165e28d9c7391ae10676aa4e
aksh-t <tanno.ao@gmail.com>
2020-09-19 02:24:28 +0900

    discountも中間データ構造に移す

--- catalog/chap6/SplitPhase/main.js
+++ catalog/chap6/SplitPhase/main.js
@@ -3,17 +3,17 @@ exports.priceOrder = function priceOrder(product, quantity, shippingMethod) {
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice * product.discountRate;
 
-  const priceData = { basePrice: basePrice, quantity: quantity };
+  const priceData = { basePrice: basePrice, quantity: quantity, discount: discount };
 
-  const price = applyShipping(priceData, shippingMethod, discount);
+  const price = applyShipping(priceData, shippingMethod);
   return price;
 };
 
 // テキストでは `送料の計算` と書いてあるが、実際やっているのは `注文の合計料金の計算` では？
-function applyShipping(priceData, shippingMethod, discount) {
+function applyShipping(priceData, shippingMethod) {
   const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
     ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = priceData.quantity * shippingPerCase;
-  const price = priceData.basePrice - discount + shippingCost;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
   return price;
 }
