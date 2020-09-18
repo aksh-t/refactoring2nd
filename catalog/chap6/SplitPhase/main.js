a5efa861f6075a69c8b6f7f18210d36ade5ec068
aksh-t <tanno.ao@gmail.com>
2020-09-19 02:19:26 +0900

    `送料の計算` を関数抽出する

--- catalog/chap6/SplitPhase/main.js
+++ catalog/chap6/SplitPhase/main.js
@@ -2,6 +2,12 @@ exports.priceOrder = function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice * product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  return price;
+};
+
+// テキストでは `送料の計算` と書いてあるが、実際やっているのは `注文の合計料金の計算` では？
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
     ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = quantity * shippingPerCase;
