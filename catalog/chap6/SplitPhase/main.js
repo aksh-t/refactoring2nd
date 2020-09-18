4ad7f50cdf93792a46cd8961182517cd893c6560
aksh-t <tanno.ao@gmail.com>
2020-09-19 02:20:21 +0900

    一旦、中間データ構想を空っぽの状態で導入する

--- catalog/chap6/SplitPhase/main.js
+++ catalog/chap6/SplitPhase/main.js
@@ -2,12 +2,15 @@ exports.priceOrder = function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice * product.discountRate;
-  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+
+  const priceData = {};
+
+  const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
   return price;
 };
 
 // テキストでは `送料の計算` と書いてあるが、実際やっているのは `注文の合計料金の計算` では？
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
+function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
     ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = quantity * shippingPerCase;
