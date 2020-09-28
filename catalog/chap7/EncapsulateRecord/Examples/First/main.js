76809491a89cbda3389d460a61065a7cf8cda931
aksh-t <tanno.ao@gmail.com>
2020-09-28 09:21:48 +0900

    レコードをクラスに置き換える

--- catalog/chap7/EncapsulateRecord/Examples/First/main.js
+++ catalog/chap7/EncapsulateRecord/Examples/First/main.js
@@ -1,6 +1,15 @@
-const organization = { name: "Acme Gooseberries", country: "GB" };
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+const organization = new Organization({ name: "Acme Gooseberries", country: "GB" });
 
 function getRawDataOfOrganization() {
+  return organization._data;
+}
+function getOrganization() {
   return organization;
 }
 
