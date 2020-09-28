9f86d2998655150e2a8411f67f549563357bfdc7
aksh-t <tanno.ao@gmail.com>
2020-09-28 09:19:11 +0900

    変数のカプセル化

--- catalog/chap7/EncapsulateRecord/Examples/First/main.js
+++ catalog/chap7/EncapsulateRecord/Examples/First/main.js
@@ -1,12 +1,16 @@
 const organization = { name: "Acme Gooseberries", country: "GB" };
 
+function getRawDataOfOrganization() {
+  return organization;
+}
+
 function H1() {
-  const result = `<h1>${organization.name}</h1>`;
+  const result = `<h1>${getRawDataOfOrganization().name}</h1>`;
   return result;
 };
 
 function updateName(newName) {
-  organization.name = newName;
+  getRawDataOfOrganization().name = newName;
 };
 
 exports.organization = organization;
