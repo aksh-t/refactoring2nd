b406e766a6b996162d9c35b5c90f7fa41f1f64bf
aksh-t <tanno.ao@gmail.com>
2020-09-29 00:02:13 +0900

    参照されているusageをCustomerDataクラスの関数として抽出

--- catalog/chap7/EncapsulateRecord/Examples/EncapsulatingANestedRecord/main.js
+++ catalog/chap7/EncapsulateRecord/Examples/EncapsulatingANestedRecord/main.js
@@ -30,6 +30,10 @@ class CustomerData {
     return _.cloneDeep(this._data);
   }
 
+  usage(customerID, year, month) {
+    return this._data[customerID].usages[year][month];
+  }
+
   setUsage(customerID, year, month, amount) {
     this._data[customerID].usages[year][month] = amount;
   }
@@ -50,8 +54,8 @@ function update(customerID, year, month, amount) {
 }
 
 function compareUsage(customerID, laterYear, month) {
-  const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
-  const earlier = getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
+  const later = getCustomerData().usage(customerID, laterYear, month);
+  const earlier = getCustomerData().usage(customerID, laterYear - 1, month);
   return { laterAmount: later, change: later - earlier };
 }
 
