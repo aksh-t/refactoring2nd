5674eef167896c6ea5a6080dd94c10272744f412
aksh-t <tanno.ao@gmail.com>
2020-09-28 22:29:03 +0900

    変数のカプセル化

--- catalog/chap7/EncapsulateRecord/Examples/EncapsulatingANestedRecord/main.js
+++ catalog/chap7/EncapsulateRecord/Examples/EncapsulatingANestedRecord/main.js
@@ -19,13 +19,16 @@ const customerData = {
   }
 };
 
+function getRawDataOfCustomers() { return customerData; }
+function setRawDataOfCustomers(arg) { customerData = arg; }
+
 function update(customerID, year, month, amount) {
-  customerData[customerID].usages[year][month] = amount;
+  getRawDataOfCustomers()[customerID].usages[year][month] = amount;
 }
 
 function compareUsage(customerID, laterYear, month) {
-  const later = customerData[customerID].usages[laterYear][month];
-  const earlier = customerData[customerID].usages[laterYear - 1][month];
+  const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
+  const earlier = getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
   return { laterAmount: later, change: later - earlier };
 }
 
