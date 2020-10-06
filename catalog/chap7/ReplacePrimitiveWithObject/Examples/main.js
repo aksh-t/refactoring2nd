875d78ed3e721d97482efddb4da9880ea706cc69
aksh-t <tanno.ao@gmail.com>
2020-10-07 00:51:26 +0900

    インスタンス変数priorityのカプセル化

--- catalog/chap7/ReplacePrimitiveWithObject/Examples/main.js
+++ catalog/chap7/ReplacePrimitiveWithObject/Examples/main.js
@@ -3,6 +3,9 @@ class Order {
     this.priority = data.priority;
     // 初期化が続く
   }
+
+  get priority() { return this._priority; }
+  set priority(aString) { this._priority = aString; }
 }
 
 // Orderクラスを利用する側のコード（クライアントコード）
