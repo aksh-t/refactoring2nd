8f7eaa18769b2ff3a11ea2c861b21e33f0d48177
aksh-t <tanno.ao@gmail.com>
2020-10-17 18:27:26 +0900

    discountFactor の算出ロジックを関数(getter)に抽出する

--- catalog/chap7/ReplaceTempWithQuery/Examples/main.js
+++ catalog/chap7/ReplaceTempWithQuery/Examples/main.js
@@ -13,6 +13,12 @@ class Order {
   get basePrice() {
     return this._quantity * this._item.price;
   }
+
+  get discountFactor() {
+    var discountFactor = 0.98;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return discountFactor;
+  }
 }
 
 exports.Order = Order;
