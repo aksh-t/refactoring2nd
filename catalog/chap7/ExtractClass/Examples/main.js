741f43547e4d9b63dc0bfdc5133ebcfabf1c4bdd
aksh-t <tanno.ao@gmail.com>
2020-10-21 01:09:41 +0900

    telephoneNumber()の関数名を変更

--- catalog/chap7/ExtractClass/Examples/main.js
+++ catalog/chap7/ExtractClass/Examples/main.js
@@ -12,7 +12,7 @@ class Person {
   }
   get name() { return this._name; }
   set name(arg) { this._name = arg; }
-  get telephoneNumber() { return this._telephoneNumber.telephoneNumber; }
+  get telephoneNumber() { return this._telephoneNumber.toString(); }
   get officeAreaCode() { return this._telephoneNumber.areaCode; }
   set officeAreaCode(arg) { this._telephoneNumber.areaCode = arg; }
   get officeNumber() { return this._telephoneNumber.number; }
@@ -24,7 +24,7 @@ class TelephoneNumber {
     this._areaCode = data.officeAreaCode;
     this._number = data.officeNumber;
   }
-  get telephoneNumber() { return `(${this.areaCode}) ${this.number}`; }
+  toString() { return `(${this.areaCode}) ${this.number}`; }
   get areaCode() { return this._areaCode; }
   set areaCode(arg) { this._areaCode = arg; }
   get number() { return this._number; }
