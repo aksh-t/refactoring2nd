17d7a432097f8d8e1da7d6ff59575b0c8066b5b2
aksh-t <tanno.ao@gmail.com>
2020-10-21 00:21:00 +0900

    クラスの抽出: 例の最初の状態

---
+++ catalog/chap7/ExtractClass/Examples/main.js
@@ -0,0 +1,16 @@
+class Person {
+  constructor(data) {
+    this._name = data.name;
+    this._officeAreaCode = data.officeAreaCode;
+    this._officeNumber = data.officeNumber;
+  }
+  get name() { return this._name; }
+  set name(arg) { this._name = arg; }
+  get telephoneNumber() { return `(${this.officeAreaCode}) ${this.officeNumber}`; }
+  get officeAreaCode() { return this._officeAreaCode; }
+  set officeAreaCode(arg) { this._officeAreaCode = arg; }
+  get officeNumber() { return this._officeNumber; }
+  set officeNumber(arg) { this._officeNumber = arg; }
+}
+
+exports.Person = Person;
---
+++ catalog/chap7/ExtractClass/Examples/test.js
@@ -0,0 +1,42 @@
+const expect = require('chai').expect;
+const { Person } = require('./main');
+
+function getPersonData() {
+  return { name: "Martin", officeAreaCode: "03", officeNumber: "12345678" };
+}
+
+function createPerosn() {
+  return new Person(getPersonData());
+}
+
+describe('Person', () => {
+  it('create instance', () => {
+    // コンストラクタの動きをテストする意図を伝えるため、
+    // あえてcreatePerosn()を使わない
+    const data = getPersonData();
+    const aPerson = new Person(data);
+    expect(aPerson).to.be.an.instanceof(Person);
+  });
+  describe('officeAreaCode', () => {
+    it('getter', () => {
+      const aPerson = createPerosn();
+      expect(aPerson.officeAreaCode).to.equal("03");
+    });
+    it('setter', () => {
+      const aPerson = createPerosn();
+      aPerson.officeAreaCode = '30';
+      expect(aPerson.officeAreaCode).to.equal("30");
+    });
+  });
+  describe('officeNumber', () => {
+    it('getter', () => {
+      const aPerson = createPerosn();
+      expect(aPerson.officeNumber).to.equal("12345678");
+    });
+    it('setter', () => {
+      const aPerson = createPerosn();
+      aPerson.officeNumber = '23456789';
+      expect(aPerson.officeNumber).to.equal("23456789");
+    });
+  });
+});
