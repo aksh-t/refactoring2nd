41759cf797703c43487ef29db738afc43860a836
aksh-t <tanno.ao@gmail.com>
2020-10-21 01:07:11 +0900

    TelephoneNumberクラスの変数名、メソッド名の変更

--- catalog/chap7/ExtractClass/Examples/main.js
+++ catalog/chap7/ExtractClass/Examples/main.js
@@ -13,22 +13,22 @@ class Person {
   get name() { return this._name; }
   set name(arg) { this._name = arg; }
   get telephoneNumber() { return this._telephoneNumber.telephoneNumber; }
-  get officeAreaCode() { return this._telephoneNumber.officeAreaCode; }
-  set officeAreaCode(arg) { this._telephoneNumber.officeAreaCode = arg; }
-  get officeNumber() { return this._telephoneNumber.officeNumber; }
-  set officeNumber(arg) { this._telephoneNumber.officeNumber = arg; }
+  get officeAreaCode() { return this._telephoneNumber.areaCode; }
+  set officeAreaCode(arg) { this._telephoneNumber.areaCode = arg; }
+  get officeNumber() { return this._telephoneNumber.number; }
+  set officeNumber(arg) { this._telephoneNumber.number = arg; }
 }
 
 class TelephoneNumber {
   constructor(data) {
-    this._officeAreaCode = data.officeAreaCode;
-    this._officeNumber = data.officeNumber;
+    this._areaCode = data.officeAreaCode;
+    this._number = data.officeNumber;
   }
-  get telephoneNumber() { return `(${this.officeAreaCode}) ${this.officeNumber}`; }
-  get officeAreaCode() { return this._officeAreaCode; }
-  set officeAreaCode(arg) { this._officeAreaCode = arg; }
-  get officeNumber() { return this._officeNumber; }
-  set officeNumber(arg) { this._officeNumber = arg; }
+  get telephoneNumber() { return `(${this.areaCode}) ${this.number}`; }
+  get areaCode() { return this._areaCode; }
+  set areaCode(arg) { this._areaCode = arg; }
+  get number() { return this._number; }
+  set number(arg) { this._number = arg; }
 }
 
 exports.Person = Person;
