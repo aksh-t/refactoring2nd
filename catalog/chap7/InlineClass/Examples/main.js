51b489aba24085924d415f39e4808fcfa29383c9
aksh-t <tanno.ao@gmail.com>
2020-10-28 00:44:15 +0900

    shippingCompanyフィールドをTrackingInformationクラスからShipmentクラスに移す

--- catalog/chap7/InlineClass/Examples/main.js
+++ catalog/chap7/InlineClass/Examples/main.js
@@ -1,11 +1,8 @@
 class TrackingInformation {
   // テキストにはコンストラクタは書いてないので、自分で仕様を決めて実装した
   constructor(data) {
-    this._shippingCompany = data.shippingCompany;
     this._trackingNumber = data.trackingNumber;
   }
-  get shippingCompany() { return this._shippingCompany; }
-  set shippingCompany(arg) { this._shippingCompany = arg; }
   get trackingNumber() { return this._trackingNumber; }
   set trackingNumber(arg) { this._trackingNumber = arg; }
 }
@@ -14,6 +11,7 @@ class Shipment {
   // テキストにはコンストラクタは書いてないので、自分で仕様を決めて実装した
   constructor(data) {
     this._trackingInformation = new TrackingInformation(data);
+    this._shippingCompany = data.shippingCompany;
   }
   get trackingInfo() {
     return `${this.shippingCompany}: ${this.trackingNumber}`;
@@ -22,12 +20,8 @@ class Shipment {
   set trackingInformation(aTrackingInformation) {
     this._trackingInformation = aTrackingInformation;
   }
-  get shippingCompany() {
-    return this._trackingInformation.shippingCompany;
-  }
-  set shippingCompany(arg) {
-    this._trackingInformation.shippingCompany = arg;
-  }
+  get shippingCompany() { return this._shippingCompany; }
+  set shippingCompany(arg) { this._shippingCompany = arg; }
   get trackingNumber() {
     return this._trackingInformation.trackingNumber;
   }
