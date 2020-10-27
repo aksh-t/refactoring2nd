0f964ff4680f5ba02313a2925ed96b15e860f8f3
aksh-t <tanno.ao@gmail.com>
2020-10-28 00:28:50 +0900

    TrackingInformationのメソッド呼び出し箇所を、テストで擬似的に表現する

--- catalog/chap7/InlineClass/Examples/test.js
+++ catalog/chap7/InlineClass/Examples/test.js
@@ -10,3 +10,19 @@ describe('Shipment', () => {
     expect(aShipment.trackingInfo).to.equal('Hoge Express: 1000');
   });
 });
+
+// callerをテストで擬似的に表現する
+describe('caller', () => {
+  it('', () => {
+    const aShipment = new Shipment({
+      shippingCompany: 'Hoge Express',
+      trackingNumber: 1000
+    });
+    const request = { vendor: 'Fuga International Trade' };
+
+    // TrackingInformation のメソッドを呼んでいる箇所
+    aShipment.trackingInformation.shippingCompany = request.vendor;
+    expect(aShipment.trackingInformation.shippingCompany).to
+      .equal('Fuga International Trade');
+  });
+});
