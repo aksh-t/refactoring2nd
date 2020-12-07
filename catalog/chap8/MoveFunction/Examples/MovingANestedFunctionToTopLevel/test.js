9a053452be14f4408fa244108edecff3cb7d3f82
aksh-t <tanno.ao@gmail.com>
2020-12-08 00:55:48 +0900

    distance関連の関数を本実装したのに伴い、テストを調整

--- catalog/chap8/MoveFunction/Examples/MovingANestedFunctionToTopLevel/test.js
+++ catalog/chap8/MoveFunction/Examples/MovingANestedFunctionToTopLevel/test.js
@@ -7,8 +7,8 @@ describe('', () => {
     const point2 = { lon: 140, lat: 36 };
     expect(trackSummary([point1, point2])).to.deep.equal({
       time: 60,
-      distance: 1,
-      pace: 1
+      distance: 89.09934386910743,
+      pace: 0.011223427205805952
     });
   });
 });
