1306ba15428f0591be07f717abbb164a3e7d41bb
aksh-t <tanno.ao@gmail.com>
2020-12-08 01:20:32 +0900

    関数の移動: クラス間での移動の例の最初の状態

---
+++ catalog/chap8/MoveFunction/Examples/MovingBetweenClasses/main.js
@@ -0,0 +1,42 @@
+class Account {
+  constructor(daysOverdrawn, typeCode) {
+    this._daysOverdrawn = daysOverdrawn;
+    this._type = new AccountType(typeCode);
+  }
+  get daysOverdrawn() {
+    return this._daysOverdrawn;
+  }
+  get type() {
+    return this._type;
+  }
+
+  get bankCharge() {
+    let result = 4.5;
+    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    return result;
+  }
+
+  get overdraftCharge() {
+    if (this.type.isPremium) {
+      const baseCharege = 10;
+      if (this.daysOverdrawn <= 7)
+        return baseCharege;
+      else
+        return baseCharege + (this.daysOverdrawn - 7) * 0.85;
+    }
+    else
+      return this.daysOverdrawn * 1.75;
+  }
+}
+
+class AccountType {
+  constructor(code) {
+    this._code = code;
+  }
+  get code() {
+    return this._code;
+  }
+  get isPremium() {
+    return this.code === "09";
+  }
+}
