e6f3d06863110c4982f266c46a7f7b25cc47d12f
aksh-t <tanno.ao@gmail.com>
2020-12-07 12:44:49 +0900

    関数の移動: 入れ子関数の例の最初の状態

---
+++ catalog/chap8/MoveFunction/Examples/MovingANestedFunctionToTopLevel/main.js
@@ -0,0 +1,28 @@
+function trackSummary(points) {
+  const totalTime = calculateTime();
+  const totalDistance = calculateDistance();
+  const pace = totalTime / 60 / totalDistance;
+  return {
+    time: totalTime,
+    distance: totalDistance,
+    pace: pace
+  };
+
+  function calculateDistance() {
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+      result += distance(points[i - 1], points[i]);
+    }
+    return result;
+  }
+
+  function distance(p1, p2) {
+    // ...
+  }
+  function radians(degrees) {
+    // ...
+  }
+  function calculateTime() {
+    // ...
+  }
+}
